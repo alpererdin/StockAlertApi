55799b1b2c50279172107dad52b03b42a000b97b
alpererdin <alpererdin.de@hotmail.com>
2026-04-02 18:47:58 +0200

    test: add unit tests for alerts service

--- tests/StockAlertApi.Tests/Services/AlertsServiceTests.cs
+++ tests/StockAlertApi.Tests/Services/AlertsServiceTests.cs
@@ -13,7 +13,6 @@ public class AlertsServiceTests
     [Fact]
     public async Task CreateAlertAsync_WhenDuplicateExists_ReturnsNull()
     {
-        // Arrange
         var mockRepo = new Mock<IAlertRepository>();
         var userId = Guid.NewGuid();
         var stockId = Guid.NewGuid();
@@ -32,18 +31,15 @@ public class AlertsServiceTests
 
         var service = new AlertsService(mockRepo.Object);
 
-        // Act
         var result = await service.CreateAlertAsync(
             userId, stockId, 100m, AlertDirection.Above);
 
-        // Assert
         result.Should().BeNull();
     }
 
     [Fact]
     public async Task CreateAlertAsync_WhenNoDuplicate_CreatesAlert()
     {
-        // Arrange
         var mockRepo = new Mock<IAlertRepository>();
         var userId = Guid.NewGuid();
         var stockId = Guid.NewGuid();
@@ -53,13 +49,99 @@ public class AlertsServiceTests
 
         var service = new AlertsService(mockRepo.Object);
 
-        // Act
         var result = await service.CreateAlertAsync(
             userId, stockId, 100m, AlertDirection.Above);
 
-        // Assert
         result.Should().NotBeNull();
         result!.TargetPrice.Should().Be(100m);
+        mockRepo.Verify(r => r.AddAsync(It.IsAny<Alert>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateAlertAsync_WhenSameStockButDifferentDirection_CreatesAlert()
+    {
+        var mockRepo = new Mock<IAlertRepository>();
+        var userId = Guid.NewGuid();
+        var stockId = Guid.NewGuid();
+
+        mockRepo.Setup(r => r.GetUserAlertsAsync(userId))
+            .ReturnsAsync(new List<Alert>
+            {
+                new Alert
+                {
+                    StockId = stockId,
+                    TargetPrice = 100m,
+                    Direction = AlertDirection.Above,
+                    Status = AlertStatus.Active
+                }
+            });
+
+        var service = new AlertsService(mockRepo.Object);
+
+        var result = await service.CreateAlertAsync(
+            userId, stockId, 100m, AlertDirection.Below);
+
+        result.Should().NotBeNull();
+        result!.Direction.Should().Be(AlertDirection.Below);
+        mockRepo.Verify(r => r.AddAsync(It.IsAny<Alert>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateAlertAsync_WhenSameUserButDifferentStock_CreatesAlert()
+    {
+        var mockRepo = new Mock<IAlertRepository>();
+        var userId = Guid.NewGuid();
+        var existingStockId = Guid.NewGuid();
+        var newStockId = Guid.NewGuid();
+
+        mockRepo.Setup(r => r.GetUserAlertsAsync(userId))
+            .ReturnsAsync(new List<Alert>
+            {
+                new Alert
+                {
+                    StockId = existingStockId,
+                    TargetPrice = 100m,
+                    Direction = AlertDirection.Above,
+                    Status = AlertStatus.Active
+                }
+            });
+
+        var service = new AlertsService(mockRepo.Object);
+
+        var result = await service.CreateAlertAsync(
+            userId, newStockId, 100m, AlertDirection.Above);
+
+        result.Should().NotBeNull();
+        result!.StockId.Should().Be(newStockId);
+        mockRepo.Verify(r => r.AddAsync(It.IsAny<Alert>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateAlertAsync_WhenExistingAlertIsInactive_CreatesAlert()
+    {
+        var mockRepo = new Mock<IAlertRepository>();
+        var userId = Guid.NewGuid();
+        var stockId = Guid.NewGuid();
+
+        mockRepo.Setup(r => r.GetUserAlertsAsync(userId))
+            .ReturnsAsync(new List<Alert>
+            {
+                new Alert
+                {
+                    StockId = stockId,
+                    TargetPrice = 100m,
+                    Direction = AlertDirection.Above,
+                    Status = AlertStatus.Triggered
+                }
+            });
+
+        var service = new AlertsService(mockRepo.Object);
+
+        var result = await service.CreateAlertAsync(
+            userId, stockId, 100m, AlertDirection.Above);
+
+        result.Should().NotBeNull();
+        result!.Status.Should().Be(AlertStatus.Active);
         mockRepo.Verify(r => r.AddAsync(It.IsAny<Alert>()), Times.Once);
     }
 }
